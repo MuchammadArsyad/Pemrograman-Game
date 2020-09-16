fe29eb10a9aa40f30f5896c6819731bfbe6a6c67
Muchammad Arsyad <arsyad1822@gmail.com>
2020-09-16 12:07:47 +0700

    Initial commit

---
+++ VR Pertemuan 2/Assets/UbahWarna2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna2 : MonoBehaviour
+{
+    public void Merah(){
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void Hijau(){
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void Putih(){
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
---
+++ VR Pertemuan 2/Assets/UbahWarna3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna3 : MonoBehaviour
+{
+    public void Biru(){
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+    public void Hitam(){
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+    public void Merah(){
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
