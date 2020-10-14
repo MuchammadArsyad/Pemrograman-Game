822f417cc8a329bb6570a8ae974874bc9be4e707
Muchammad Arsyad <arsyad1822@gmail.com>
2020-10-14 22:42:04 +0700

    update

--- VR Pertemuan 5/Assets/TangkapObjek.cs
+++ VR Pertemuan 5/Assets/TangkapObjek.cs
@@ -1,38 +1,22 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class TangkapObjek : MonoBehaviour
-// {
-//     public GameObject bola;
-//     public GameObject tangan;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if(input.GetButtonDown("Fire1"))
-//         {
-//             bola.transform.SetParent(tangan.transform);
-//             bola.transform.localPosition=tangan.transform.localPosition;
-//         }
-//     }
-// }A
-
-public GameObject bola;
+public class TangkapObjek : MonoBehaviour
+{
+    public GameObject bola;
     public GameObject tangan;
-    bool tertangkap = false;
-    Vector3 posisiBola;
 
+    bool tertangkap = false;
+
+    Vector3 posisiBola;
+    // Start is called before the first frame update
     void Start()
     {
         posisiBola = bola.transform.position;
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -40,15 +24,18 @@ public GameObject bola;
             if (!tertangkap)
             {
                 bola.transform.SetParent(tangan.transform);
-                bola.transform.localPosition = new Vector3(0f, -672f, 0f);
+                bola.transform.localPosition = new Vector3(0f, -.672f, 0f);   
                 tertangkap = true;
-   
+                bola.GetComponent<Renderer>().material.color = Color.blue;
             }
             else if (tertangkap)
             {
                 bola.transform.SetParent(null);
                 bola.transform.localPosition = posisiBola;
                 tertangkap = false;
+                bola.GetComponent<Renderer>().material.color = Color.red;
             }
+            
         }
     }
+}
